84eca41489cc54741fb409fbebed68520c66019c
Aaron Hedges <aaron@dashron.com>
2014-03-03 12:18:13 -0500

    fix auth example to match the new 'always an array' return changes

--- example/auth.php
+++ example/auth.php
@@ -36,7 +36,7 @@ if (preg_match('%^/callback%', $_SERVER["REQUEST_URI"])) {
 	$lib = new Vimeo($config['client_id'], $config['client_secret']);
 	$tokens = $lib->accessToken($_GET['code'], REDIRECT_URI);
 	if ($tokens['status'] == 200) {
-		$_SESSION['access_token'] = $tokens['body']->access_token;
+		$_SESSION['access_token'] = $tokens['body']['access_token'];
 		echo 'Successful authentication. Please go to <a href="http://localhost:8080">localhost:8080</a>';
 	} else {
 		echo "Unsuccessful authentication";
@@ -52,14 +52,12 @@ if (preg_match('%^/callback%', $_SERVER["REQUEST_URI"])) {
 	if (empty($_SESSION['access_token'])) {
 		echo "This is an unauthenticated request to /users/dashron<br />";
 		$lib = new Vimeo($config['client_id'], $config['client_secret']);
-		$user = $lib->request('/users/dashron');
 		$_SESSION['state'] = base64_encode(openssl_random_pseudo_bytes(30));
 
 		echo 'To authenticate you should click <a href="'
 			. $lib->buildAuthorizationEndpoint(REDIRECT_URI, 'public', $_SESSION['state'])
 			. '">here</a><br />';
 
-		var_dump($user);
 	} else {
 		echo "This is an authenticated request to /me<br />";
 		echo 'To start over click <a href="http://localhost:8080/reset">here</a><br />';
