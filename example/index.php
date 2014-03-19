39f8f7fdce9f5d7472a745d79c2aab62265417f9
Vincent Klaiber <vincentklaiber@users.noreply.github.com>
2014-03-19 10:48:34 +0100

    Remove use Vimeo and add Vimeo\Vimeo.

--- example/index.php
+++ example/index.php
@@ -19,10 +19,8 @@ error_reporting(E_ALL);
 
 require_once('vendor/autoload.php');
 
-use Vimeo;
-
 $config = json_decode(file_get_contents('./config.json'), true);
 
-$lib = new Vimeo($config['client_id'], $config['client_secret']);
+$lib = new Vimeo\Vimeo($config['client_id'], $config['client_secret']);
 $user = $lib->request('/users/dashron');
 print_r($user);
