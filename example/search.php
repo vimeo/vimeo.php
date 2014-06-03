9f60d2933de92fdab89cb40dff2d2c08872ef3e8
Joe Cabrera <calcmaster16@gmail.com>
2014-06-03 21:02:25 +0000

    more barebones example, also sending the parameters via an assoc array

--- example/search.php
+++ example/search.php
@@ -24,33 +24,6 @@ $config = json_decode(file_get_contents('./config.json'), true);
 $lib = new Vimeo($config['client_id'], $config['client_secret'], $config['access_token']);
 
 // Set the number of items to show on each page to 50
-$search_results = $lib->request('/videos?per_page=50&query='.urlencode($_GET['query']));
+$search_results = $lib->request('/videos', array('per_page' => 50, 'query' => urlencode($_GET['query'])));
 
-$videos = $search_results['body']['data'];
-
-// Select only the elements from the API call we need
-$out=array_map(function($x) {return array('link' => $x['link'], 'name' => $x['name'], 'user' => $x['user']['name']);}, $videos);
-
-// Our filter for only search hits from myusername
-class validItems extends FilterIterator
-{
-    public function accept()
-    {
-        $current = $this->current();
-        if ($current['user'] == 'myusername') {
-            return true;
-        }
-        return false;
-    }
-}
-
-// Apply the filter on our search results
-$available = new validItems(new ArrayIterator($out));
-
-// Create json encoded output
-$out = array();
-foreach ($available as $value) {
-    array_push($out, $value);
-}
-
-echo json_encode($out);
+print_r($search_results);
