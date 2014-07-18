beb9abf23090665e242623398e838706460f9965
Joe Cabrera <joec@vimeo.com>
2014-07-18 16:34:09 -0400

    vod film and series example

--- example/vod.php
+++ example/vod.php
@@ -18,19 +18,67 @@ if (empty($config['access_token'])) {
 
 $lib = new Vimeo($config['client_id'], $config['client_secret'], $config['access_token']);
 
+// VOD film example
+
 // Create a new vod page
-$create_vod_page = $lib->request('/me/ondemand/pages', array('name' => 'ohhai', 'type' => 'film', 'content_rating' => 'safe', 'link' => 'ohhai', 'dommain_link' => 'ohhai', 'rent.active' => true, 'rent.price.USD' => 1.0, 'rent.period' => 'week', 'buy.active' => true, 'episodes.rent.active' => true, 'episodes.rent.price.USD' => 1.0, 'episode.rent.period' => 'week', 'episodes.buy.active' => true, 'episodes.buy.price.USD' => 1.0), 'POST');
+$create_vod_film = $lib->request('/me/ondemand/pages', array('name' => 'myfilm', 'type' => 'film', 'content_rating' => 'safe', 'link' => 'myfilm', 'dommain_link' => 'myfilm', 'rent' => array('active' => true, 'price' => array('USD' => 5.0), 'period' => '24 hour'), 'buy' => array('active' => true, 'price' => array('USD' => 10.0))), 'POST');
 
-print_r($create_vod_page);
+// Set a Genre
+$genre = $lib->request('/ondemand/pages/myfilm/genres/art', array(), 'PUT');
 
-// Add a video to the newly created vod page
-$add_video = $lib->request('/ondemand/pages/ohhai/videos/27687226', array('type' => 'main', 'rent.active' => true, 'rent.price.USD' => 1.0, 'buy.active' => true, 'buy.price.USD' => 2.0), 'PUT');
+// Add a video
+$uri = $lib->upload('myvideo.mp4');
+$video_data = $lib->request($uri);
+$film_video = $lib->request('/ondemand/pages/myfilm'.$video_data['body']['uri'], array('type' => 'main'), 'PUT');
 
-print_r($add_video);
+// Add a trailer
+$uri = $lib->upload('mytrailer.mp4');
+$video_data = $lib->request($uri);
+$film_trailer = $lib->request('/ondemand/pages/myfilm'.$video_data['body']['uri'], array('type' => 'trailer'), 'PUT');
 
-// Check to make sure the new video has been added to the vod page properly
-$check_video = $lib->request('/ondemand/pages/ohhai/videos', array('filter' => 'all', 'sort' => 'default'));
-
+// Check to make sure the new video and trailer has been added to the vod page properly
+$check_video = $lib->request('/ondemand/pages/myfilm/videos', array('filter' => 'all', 'sort' => 'default'));
 print_r($check_video);
+
+// Add a picture to our vod page
+$response = $lib->uploadImage('/ondemand/pages/myfilm/pictures', './test.png');
+$poster = $lib->request($response, array('active' => true), 'PATCH');
+
+// Publish our new vod page - You can only publish after you all videos for your film have finished transcoding
+//$publish_video = $lib->request('/ondemand/pages/myfilm', array('publish' => array('active' => true)), 'PATCH');
+//print_r($publish_video);
+
+// VOD series example
+
+// Create a new vod series
+$create_vod_series = $lib->request('/me/ondemand/pages', array('name' => 'myseries', 'type' => 'series', 'content_rating' => 'safe', 'link' => 'myseries', 'dommain_link' => 'myseries', 'rent' => array('active' => true, 'price' => array('USD' => 5.0), 'period' => '24 hour'), 'buy' => array('active' => true, 'price' => array('USD' => 10.0)), 'episodes' => array('rent' => array('active' => true, 'price' => array('USD' => 1.0), 'period' => '48 hour'), 'buy' => array('active' => true, 'price' => array('USD' => 2.0)))), 'POST');
+
+// Set a Genre
+$genre = $lib->request('/ondemand/pages/myseries/genres/art', array(), 'PUT');
+
+// Add some videos
+$uri = $lib->upload('myvideo1.mp4');
+$video_data = $lib->request($uri);
+$series_video_1 = $lib->request('/ondemand/pages/myseries'.$video_data['body']['uri'], array('type' => 'main'), 'PUT');
+$uri = $lib->upload('myvideos2.mp4');
+$video_data = $lib->request($uri);
+$series_video_2 = $lib->request('/ondemand/pages/myseries'.$video_data['body']['uri'], array('type' => 'main', 'rent' => array('active' => true, 'price' => array('USD' => 3)), 'buy' => array('active' => true, 'price' => array('USD' => 4))), 'PUT');
+
+// Add a trailer
+$uri = $lib->upload('mytrailer.mp4');
+$video_data = $lib->request($uri);
+$series_trailer = $lib->request('/ondemand/pages/myseries/'.$video_data['body']['uri'], array('type' => 'trailer'), 'PUT');
+
+// Check to make sure the new video and trailer has been added to the vod page properly
+$check_series_video = $lib->request('/ondemand/pages/myseries/videos', array('filter' => 'all', 'sort' => 'default'));
+print_r($check_series_video);
+
+// Add a picture to our vod page
+$response = $lib->uploadImage('/ondemand/pages/myseries/pictures', './test.png');
+$poster = $lib->request($response, array('active' => true), 'PATCH');
+
+// Publish our new vod page - You can only publish after you all videos for your series have finished transcoding
+//$publish_video = $lib->request('/ondemand/pages/myseries', array('publish' => array('active' => true)), 'PATCH');
+//print_r($publish_video);
 
 ?>
