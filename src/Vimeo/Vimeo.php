229226355b34bf5d10adf2e07aa42e5eb235bc4a
Kenya S <kenya@vimeo.com>
2015-04-21 13:01:57 -0400

    Use single quotes for consistency

--- src/Vimeo/Vimeo.php
+++ src/Vimeo/Vimeo.php
@@ -348,7 +348,7 @@ class Vimeo
     {
         if ($ticket['status'] != 201) {
             $ticket_error = $ticket['body']['error'];
-            $ticket_error = !empty($ticket_error) ? "[" . $ticket_error . "]" : '';
+            $ticket_error = !empty($ticket_error) ? '[' . $ticket_error . ']' : '';
             throw new VimeoUploadException('Unable to get an upload ticket.' . $ticket_error);
         }
 
